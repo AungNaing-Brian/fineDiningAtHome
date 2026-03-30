99b5e6c5ef8e164cbabe26805efe542a9fda9d22
Aung Naing <aungnaing.brian@gmail.com>
2026-03-30 10:47:28 +0400

    uploadedRawDesignOfHeroProfileAboutMenuAndVideos

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.hero-content', { delay: 200 });
+ScrollReveal().reveal('.card', { interval: 200 });
+ScrollReveal().reveal('.menu-item', { interval: 100 });
+ScrollReveal().reveal('.video', { interval: 200 });
